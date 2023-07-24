93dce7deb98bb6503eec01649afc17e8534948ef
kishor Rathod <99723242+kishorathod@users.noreply.github.com>
2023-07-24 08:25:05 +0000

    Data types in js

---
+++ 01_basics_of_javascript/Datatypes.js
@@ -0,0 +1,26 @@
+ // different types of  data types in javascript 
+//  let name= "mota";
+//  let age = 18 ;
+//  let isLogedIn = false;
+ 
+// primitive datatypes 
+ // bb , nn , ss , u 
+
+ // number => 2 to  power 53 
+ // bigint 
+ // string => " "
+ // boolean => true/false
+ // null => standolone value
+ // undefinded => which mean value is not assined 
+ // symbol => unique 
+
+ // user defined datatype 
+// objects => its is a user defined datatype
+
+// some annonomous behaviour while finding the type of the data 
+
+// console.log(typeof null); // this will return a object as output
+// console.log(typeof undefined); // this will return undefined as output
+
+// here is the conclusion null is an object 
+
